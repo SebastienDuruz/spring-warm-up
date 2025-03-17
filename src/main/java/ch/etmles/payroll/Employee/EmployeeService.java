d1d581140e55ae13605fcba6c9fce4838f17065d
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-03-17 12:16:35 +0100

    Docs: Add EmployeeService sequence diagram

--- src/main/java/ch/etmles/payroll/Employee/EmployeeService.java
+++ src/main/java/ch/etmles/payroll/Employee/EmployeeService.java
@@ -3,18 +3,22 @@ package ch.etmles.payroll.Employee;
 import ch.etmles.payroll.Department.Department;
 import ch.etmles.payroll.Department.DepartmentNotFoundException;
 import ch.etmles.payroll.Department.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeService {
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
 
+    @Autowired
     public EmployeeService(DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
     }
 
+    @Transactional
     public Employee hireEmployee(Long employeeId, Long departmentId) {
 
         Department department = departmentRepository.findById(departmentId)
@@ -27,6 +31,7 @@ public class EmployeeService {
             }).orElseThrow(() -> new EmployeeNotFoundException(employeeId));
     }
 
+    @Transactional
     public Employee releaseEmployee(Long employeeId) {
         return employeeRepository.findById(employeeId)
             .map(employee -> {
